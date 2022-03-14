c1b3ae12df8ae2529b6c6d6a49f42c69ea209bdc
albotero <101154154+albotero@users.noreply.github.com>
2022-03-13 21:01:27 -0500

    Comments for each function

--- run.py
+++ run.py
@@ -3,11 +3,13 @@
 import os
 import requests
 
+# Sets constants
 feedback_dir = '/data/feedback/'
 url_post = 'http://34.72.204.110/feedback/'
 keys = ('title', 'name', 'date', 'feedback')
 
 def process_file(file):
+   '''Reads contents from a text file and creates a directory with each line in keys tuple'''
    entry = {}
    with open(feedback_dir + file, 'r') as f:
       for key in keys:
@@ -16,6 +18,7 @@ def process_file(file):
    return entry
 
 def post_data(data):
+   '''Posts the dictionary to the web server'''
    print('Sending feedbacks', end = '... ')
    try:
       resp = requests.post(url_post, data)
@@ -29,6 +32,7 @@ def post_data(data):
       print(e)
 
 def main():
+   '''Iterates through the files in the target dir to process them'''
    for file in os.listdir(feedback_dir):
       data = process_file(file)
       post_data(data)
